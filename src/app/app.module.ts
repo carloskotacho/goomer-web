cdfcc1bbe4ad475b0d23126a657d746cb31f70f7
Carlos - PC <carloskotachoamd@hotmail.com>
2020-11-01 21:16:44 -0300

    scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { TabViewModule } from 'primeng/tabview';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+
+    TabViewModule,
+
     AppRoutingModule
   ],
   providers: [],
