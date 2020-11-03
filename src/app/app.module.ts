c0ab91a049926c99e20384a9c4b9315b02accfd0
Carlos - PC <carloskotachoamd@hotmail.com>
2020-11-03 16:44:27 -0300

    feat: add restaurant store component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { MenubarModule } from 'primeng/menubar';
 import { CardModule } from 'primeng/card';
@@ -8,21 +9,27 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CalendarModule } from 'primeng/calendar';
+import { ChipsModule } from 'primeng/chips';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RestaurantSearchComponent } from './restaurant-search/restaurant-search.component';
+import { RestaurantStoreComponent } from './restaurant-store/restaurant-store.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     RestaurantSearchComponent,
+    RestaurantStoreComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
 
     MenubarModule,
     CardModule,
@@ -30,6 +37,9 @@ import { RestaurantSearchComponent } from './restaurant-search/restaurant-search
     ButtonModule,
     TableModule,
     TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    ChipsModule,
 
     AppRoutingModule
   ],
