fee467f9744ff7767464d1be5611b2387eae406f
Carlos - PC <carloskotachoamd@hotmail.com>
2020-11-02 23:24:49 -0300

    feat: add design for restaurant search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { MenubarModule } from 'primeng/menubar';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RestaurantSearchComponent } from './restaurant-search/restaurant-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    RestaurantSearchComponent,
   ],
   imports: [
     BrowserModule,
@@ -20,6 +26,10 @@ import { NavbarComponent } from './navbar/navbar.component';
 
     MenubarModule,
     CardModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
 
     AppRoutingModule
   ],
