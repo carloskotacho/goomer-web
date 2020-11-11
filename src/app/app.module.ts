fdccf93cac678db3dbb42e3fe01ad120e988f897
Carlos - Notebook <carloskotachoamd@hotmail.com>
2020-11-10 23:03:17 -0300

    feat: add module core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ProductsModule } from './products/products.module';
 
-import { MenubarModule } from 'primeng/menubar';
-import { CardModule } from 'primeng/card';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { TooltipModule } from 'primeng/tooltip';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CalendarModule } from 'primeng/calendar';
-import { ChipsModule } from 'primeng/chips';
-import { SplitButtonModule } from 'primeng/splitbutton';
+// import { CardModule } from 'primeng/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
 
+    CoreModule,
     RestaurantsModule,
     ProductsModule,
 
-    MenubarModule,
-    CardModule,
-    InputTextModule,
-    ButtonModule,
-    TableModule,
-    TooltipModule,
-    InputTextareaModule,
-    CalendarModule,
-    ChipsModule,
-    SplitButtonModule,
+    // CardModule,
 
     AppRoutingModule
   ],
