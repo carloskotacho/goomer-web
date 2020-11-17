5a080d4a940acd6bbbf9e75313df5a94b3a36db6
Carlos - PC <carloskotachoamd@hotmail.com>
2020-11-17 17:36:49 -0300

    feat: add locale pt-br

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { MenubarModule } from 'primeng/menubar';
 import { ConfirmationService } from 'primeng/api';
@@ -10,6 +13,8 @@ import { ErrorHandlerService } from './error-handler.service';
 import { RestaurantService } from './../restaurants/restaurant.service';
 import { ProductService } from './../products/product.service';
 import { NavbarComponent } from './navbar/navbar.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   imports: [
@@ -32,7 +37,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     ProductService,
     ErrorHandlerService,
 
-    ConfirmationService
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 })
 export class CoreModule { }
